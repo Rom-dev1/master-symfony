e04e674df5bab31e638c0ea2c47ca8fdd6ac3106
Matthieu Mota <matthieumota@gmail.com>
2023-03-14 11:43:52 +0100

    Form and validation

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,16 +3,38 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     #[Route('/product/create', name: 'app_product_create')]
-    public function create(EntityManagerInterface $entityManager): Response
+    public function create(Request $request, EntityManagerInterface $entityManager)
+    {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // La requête en BDD
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_product');
+        }
+
+        return $this->render('product/create.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/product/create-one', name: 'app_product_create_one')]
+    public function createOne(EntityManagerInterface $entityManager): Response
     {
         $product = new Product();
         $product->setName('Chaise');
---
+++ src/Form/ProductType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('price', MoneyType::class, [
+                // Si l'utilisateur saisit 99, on stocke 9900
+                // Si le produit a 9900 en BDD, on affiche 99.00
+                'divisor' => 100,
+            ])
+            ->add('image')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
