5ff7225f1829edf7f9bd0a7f680779a01031d840
Matthieu Mota <matthieumota@gmail.com>
2023-03-15 08:32:46 +0100

    Form React

--- assets/react/controllers/FioComponent.jsx
+++ assets/react/controllers/FioComponent.jsx
@@ -1,7 +1,26 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function (props) {
     let [show, setShow] = useState(false);
+
+    let [data, setData] = useState({name: ''});
+    let [errors, setErrors] = useState({});
+    let [success, setSuccess] = useState();
+
+    let handleChange = (event) => {
+        setData({ ...data, ...{ [event.target.name]: event.target.value } });
+    }
+
+    let handleSubmit = (event) => {
+        event.preventDefault();
+
+        setErrors({});
+
+        axios.post('/fio/create', data)
+            .then(response => setSuccess(response.data.message))
+            .catch(errors => setErrors(errors.response.data));
+    }
 
     return (
         <div className="mt-6">
@@ -12,6 +31,17 @@ export default function (props) {
                 </>
             }
             <button className="mt-6 bg-slate-600 rounded-lg text-white px-3 py-2 hover:bg-slate-400" onClick={() => setShow(!show)}>And who ?</button>
+
+            <form className="my-3" method="post" onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name" className="mr-2">Nom</label>
+                    <input type="text" name="name" id="name" value={data.name} onChange={handleChange} />
+                    {errors.name && <p className="text-red-500">{errors.name}</p>}
+                </div>
+                <button className="mt-6 bg-slate-600 rounded-lg text-white px-3 py-2 hover:bg-slate-400">Envoyer</button>
+            </form>
+
+            {success && <p className="text-emerald-500">{success}</p>}
         </div>
     );
 }
--- src/Controller/FioController.php
+++ src/Controller/FioController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class FioController extends AbstractController
 {
@@ -12,5 +14,29 @@ class FioController extends AbstractController
     public function index(): Response
     {
         return $this->render('fio/index.html.twig');
+    }
+
+    #[Route('/fio/create', name: 'app_fio_create', methods: ['POST'])]
+    public function create(Request $request)
+    {
+        $form = $this->createFormBuilder(null, ['csrf_protection' => false])
+            ->add('name', null, [
+                'constraints' => [new NotBlank()]
+            ])->getForm();
+        $form->submit(json_decode($request->getContent(), true));
+
+        if (!$form->isValid()) {
+            $errors = [];
+
+            foreach ($form->getErrors(true) as $error) {
+                $errors[$error->getOrigin()->getName()] = $error->getMessage();
+            }
+
+            return $this->json($errors, 422);
+        }
+
+        return $this->json([
+            'message' => 'Merci '.$form->getData()['name'].' !',
+        ]);
     }
 }
