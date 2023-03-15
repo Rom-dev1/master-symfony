5767259d48d7d02b2ad2f0a130c7261fe52aec81
Matthieu Mota <matthieumota@gmail.com>
2023-03-15 11:33:26 +0100

    Fixture post

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Post;
 use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -22,6 +23,16 @@ class AppFixtures extends Fixture
             $product->setPrice($faker->numberBetween(100, 2000) * 100);
             $product->setImage('https://picsum.photos/id/'.$faker->numberBetween(0, 1000).'/200/300');
             $manager->persist($product);
+        }
+
+        for ($i = 0; $i < 100; $i++) {
+            $post = new Post();
+            $post->setName($faker->sentence());
+            $post->setContent($faker->text());
+            $publishedAt = \DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-30 days', '30 days'));
+            $post->setPublishedAt($publishedAt);
+            $post->setActive($faker->boolean());
+            $manager->persist($post);
         }
 
         $manager->flush();
